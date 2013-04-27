90fa5e8ecaaf65e5306c2e8ddb01c2413c39745a
Richard Carter <richard@richardcarter.org>
2013-04-27 19:32:43 -0400

    main: pass jshint

--- js/main.js
+++ js/main.js
@@ -1,30 +1,33 @@
-(function() {
-  var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-                              window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-  window.requestAnimationFrame = requestAnimationFrame;
+(function () {
+    "use strict";
+    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+                                window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    window.requestAnimationFrame = requestAnimationFrame;
 })();
 
 var game;
-$(document).ready(function(){
-    require(["js/game.js"], function(Game) {
-        $('#stop-button').button('disable');
-        $('#start-button').button('enable');
-        $('#start-button').button().click(function(event) {
-            $(this).button('disable');
-            $('#stop-button').button('enable');
+$(document).ready(function () {
+    "use strict";
+
+    require(["js/game.js"], function (Game) {
+        $("#stop-button").button("disable");
+        $("#start-button").button("enable");
+        $("#start-button").button().click(function (event) {
+            $(this).button("disable");
+            $("#stop-button").button("enable");
             event.preventDefault();
             game = new Game();
-            game.Initialize('#games');
-            game.intervalHook = setInterval(function () { game.RunGameLoop() }, game.DrawInterval);
+            game.Initialize("#games");
+            game.intervalHook = setInterval(function () { game.RunGameLoop(); }, game.DrawInterval);
         });
-        $('#stop-button').button().click( function (event) {
+        $("#stop-button").button().click(function (event) {
             event.preventDefault();
-            $(this).button('disable');
+            $(this).button("disable");
             clearInterval(game.intervalHook);
             game.canvasElement.parentNode.removeChild(game.canvasElement);
             game.canvasElement = null;
             game = null;
-            $('#start-button').button('enable');
+            $("#start-button").button("enable");
         });
     });
 });
