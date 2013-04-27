4d87d151420815a510eff9f3f41107a9eb1d6349
Stephen Wright <mario.luigi.star@gmail.com>
2013-04-27 15:52:31 -0400

    Character moves, colides with the ground, and jumps. Start/Stop button added.

--- js/game.js
+++ js/game.js
@@ -1,45 +1,73 @@
 define(function() {
     function Game() {
-        var CANVAS_WIDTH = 480;
-        var CANVAS_HEIGHT = 320;
+        var CANVAS_WIDTH = 480 * 1.5;
+        var CANVAS_HEIGHT = 320 * 1.5;
         var keys = {};
         var player;
         var canvas;
         var canvasElement;
         var FPS = 30;
-        var text;
         var sprites;
+        var ground;
+        var morphyGuy;
         this.DrawInterval = 1000 / FPS;
         this.Initialize = function(where) {
             console.log('init');
             sprites = new Image();
             //sprites.src = 'sprites.png';
-            canvasElement = $("<canvas width='" + CANVAS_WIDTH + "' height='" + CANVAS_HEIGHT + "'></canvas>");
-            canvasElement.attr("id","myCanvas");
-            canvas = canvasElement.get(0).getContext("2d");
-            canvasElement.appendTo(where);
+            //canvasElement = $("<canvas width='" + CANVAS_WIDTH + "' height='" + CANVAS_HEIGHT + "'></canvas>");
+            canvasElement = document.createElement("canvas");
+            canvasElement.id = "myCanvas";
+            canvasElement.width = CANVAS_WIDTH;
+            canvasElement.height = CANVAS_HEIGHT;
+            canvas = canvasElement.getContext("2d");
+            $(where).get(0).appendChild(canvasElement);
+            this.canvasElement = canvasElement;
             //text = $.getJSON('textObject.json', function (json) { return json; });
-           
+            ground = {
+                color: "#040",
+                width: CANVAS_WIDTH,
+                height: 32,
+                x: 0,
+                y: CANVAS_HEIGHT - 32,
+                draw: function() {
+                    canvas.fillStyle = this.color;
+
+                    canvas.fillRect(this.x, this.y, this.width, this.height);
+                }
+            };
             player = {
                 game: this,
                 color: "#00A",
+                state: 'falling',
                 x: 220,
                 y: 270,
+                dx: 0,
+                dy: 0,
+                jumpHeight: 100,
+                jumpOrigin:0,
                 width: 32,
                 height: 32,
                 draw: function() {
                     canvas.fillStyle = this.color;
-                    
+
                     canvas.fillRect(this.x, this.y, this.width, this.height);
                 },
-                update: function(){}
+                update: function() {}
             };
             morphyGuy = {
                 game: this,
                 color: "#00A",
-                colors: [{value: 5, increase: true},
-                         {value: 50, increase: true},
-                         {value: 125, increase: true}],
+                colors: [{
+                    value: 5,
+                    increase: true
+                }, {
+                    value: 50,
+                    increase: true
+                }, {
+                    value: 125,
+                    increase: true
+                }],
                 colorIncrement: 3,
                 colorLimitHigh: 245,
                 colorLimitLow: 5,
@@ -49,24 +77,25 @@ define(function() {
                 height: 32,
                 draw: function() {
                     canvas.fillStyle = this.color;
-                    
+
                     canvas.fillRect(this.x, this.y, this.width, this.height);
                 },
-                update: function(){
-                    this.colors.forEach(function(item){
-                        if(item.increase)
-                          item.value+=this.colorIncrement;
-                        else
-                          item.value-=this.colorIncrement;
+                update: function() {
+                    this.colors.forEach(function(item) {
+                        if (item.increase) item.value += this.colorIncrement;
+                        else item.value -= this.colorIncrement;
 
-                        if(item.value>=this.colorLimitHigh)//may need to subtract some more if we get a weird flash
-                          item.increase = false;
-                        else if(item.value<=this.colorLimitLow)//may need to add some more if we get a weird flash
-                          item.increase = true;
-                          //if(item.value>255||item.value<0){debugger;}
+                        if (item.value >= this.colorLimitHigh) //may need to subtract some more if we get a weird flash
+                        item.increase = false;
+                        else if (item.value <= this.colorLimitLow) //may need to add some more if we get a weird flash
+                        item.increase = true;
                     }, this);
-                    console.log(this.color);//if(isNaN(this.colors[0].value)){debugger;clearInterval(this.game.intervalHook)};
-                    this.color = "#" + this.colors.map(function(item){return item.value.toString(16);}).join("");
+
+                    this.color = "#" + this.colors.map(function(item) {
+                        var hexStr = item.value.toString(16);
+                        if (hexStr.length == 1) hexStr = "0" + hexStr;
+                        return hexStr;
+                    }).join("");
                 }
             };
             this.LoadContent();
@@ -84,19 +113,33 @@ define(function() {
         this.Update = function() {
             if (keys[37]) player.x -= 5;
             if (keys[39]) player.x += 5;
-            if (keys[38]) player.y -= 5;
-            if (keys[40]) player.y += 5;
-            
+            if (keys[38] && player.state !== 'falling' && player.state !== 'jumping') {
+                player.state = "jumping";
+                player.jumpOrigin = player.y+player.height;
+            }
+            if (player.state === 'jumping') {
+                player.y -= 10;
+            }
+            if (player.jumpOrigin - player.jumpHeight >= player.y)
+                player.state = 'falling';
+            if (player.y + player.height >= ground.y) {
+                player.y += 0;
+                player.state = '';
+            }
+            else {
+                player.y += 5; //gravity!
+            }
             morphyGuy.update();
         };
 
         this.Draw = function() {
             canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
             player.draw();
-            //text.draw();
-            morphyGuy.draw()
-            canvas.fillStyle = "#000"; // Set color to black
             
+            morphyGuy.draw();
+            ground.draw();
+            canvas.fillStyle = "#000"; // Set color to black
+
         };
         $(document).keydown(function(e) {
             //console.log(e.which);
--- js/main.js
+++ js/main.js
@@ -1,12 +1,30 @@
+(function() {
+  var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+                              window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+  window.requestAnimationFrame = requestAnimationFrame;
+})();
+
+var game;
 $(document).ready(function(){
-    require(["./js/game.js"], function(Game) {
+    require(["js/game.js"], function(Game) {
+        $('#stop-button').button('disable');
+        $('#start-button').button('enable');
         $('#start-button').button().click(function(event) {
             $(this).button('disable');
+            $('#stop-button').button('enable');
             event.preventDefault();
-            console.log("click");
-            var game = new Game();
+            game = new Game();
             game.Initialize('#games');
             game.intervalHook = setInterval(function () { game.RunGameLoop() }, game.DrawInterval);
+        });
+        $('#stop-button').button().click( function (event) {
+            event.preventDefault();
+            $(this).button('disable');
+            clearInterval(game.intervalHook);
+            game.canvasElement.parentNode.removeChild(game.canvasElement);
+            game.canvasElement = null;
+            game = null;
+            $('#start-button').button('enable');
         });
     });
 });
