f3af6fb94e8c025bb40beb6c8e302bbe84f555cf
Richard Carter <richard@richardcarter.org>
2013-04-27 19:28:43 -0400

    game: pass jshint

--- js/game.js
+++ js/game.js
@@ -1,4 +1,5 @@
-define(["js/entities/player.js", "js/entities/morphyGuy.js", "js/entities/ground.js"], function(Player, MorphyGuy, Ground) {
+define(["js/entities/player.js", "js/entities/morphyGuy.js", "js/entities/ground.js"],
+        function (Player, MorphyGuy, Ground) {
     "use strict";
 
     function Game() {
@@ -13,9 +14,9 @@ define(["js/entities/player.js", "js/entities/morphyGuy.js", "js/entities/ground
         var ground;
         var morphyGuy;
         this.DrawInterval = 1000 / FPS;
-        this.Initialize = function(where) {
+        this.Initialize = function (where) {
             sprites = new Image();
-            //sprites.src = 'sprites.png';
+            //sprites.src = "sprites.png";
             canvasElement = document.createElement("canvas");
             canvasElement.id = "myCanvas";
             canvasElement.width = CANVAS_WIDTH;
@@ -23,7 +24,7 @@ define(["js/entities/player.js", "js/entities/morphyGuy.js", "js/entities/ground
             canvas = canvasElement.getContext("2d");
             $(where).get(0).appendChild(canvasElement);
             this.canvasElement = canvasElement;
-            //text = $.getJSON('textObject.json', function (json) { return json; });
+            //text = $.getJSON("textObject.json", function (json) { return json; });
             ground = new Ground();
             ground.width = CANVAS_WIDTH;
             ground.y = CANVAS_HEIGHT - 32;
@@ -31,52 +32,56 @@ define(["js/entities/player.js", "js/entities/morphyGuy.js", "js/entities/ground
             morphyGuy = new MorphyGuy();
             this.LoadContent();
         };
-        this.LoadContent = function() {
+        this.LoadContent = function () {
             // load content – graphics, sound etc.
             // since all content is loaded run main game loop
             // Calls RunGameLoop method every ‘draw interval’
         };
-        this.RunGameLoop = function() {
+        this.RunGameLoop = function () {
             this.Update();
             this.Draw();
         };
 
-        this.Update = function() {
+        this.Update = function () {
             //player.update(keys);
             morphyGuy.update(keys);
-            if (keys[37]) player.x -= 5;
-            if (keys[39]) player.x += 5;
-            if (keys[38] && player.state !== 'falling' && player.state !== 'jumping') {
-                player.state = "jumping";
-                player.jumpOrigin = player.y+player.height;
+            if (keys[37]) {
+                player.x -= 5;
             }
-            if (player.state === 'jumping') {
+            if (keys[39]) {
+                player.x += 5;
+            }
+            if (keys[38] && player.state !== "falling" && player.state !== "jumping") {
+                player.state = "jumping";
+                player.jumpOrigin = player.y + player.height;
+            }
+            if (player.state === "jumping") {
                 player.y -= 10;
             }
-            if (player.jumpOrigin - player.jumpHeight >= player.y)
-                player.state = 'falling';
-            if (player.y + player.height >= ground.y) {
-                player.state = '';
+            if (player.jumpOrigin - player.jumpHeight >= player.y) {
+                player.state = "falling";
             }
-            else {
+            if (player.y + player.height >= ground.y) {
+                player.state = "";
+            } else {
                 player.y += 5; //gravity!
             }
         };
 
-        this.Draw = function() {
+        this.Draw = function () {
             canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
             player.draw(canvas);
-            
+
             morphyGuy.draw(canvas);
             ground.draw(canvas);
             canvas.fillStyle = "#000"; // Set color to black
 
         };
-        $(document).keydown(function(e) {
+        $(document).keydown(function (e) {
             //console.log(e.which);
             keys[e.which] = true;
         });
-        $(document).keyup(function(e) {
+        $(document).keyup(function (e) {
             keys[e.which] = false;
         });
     }
